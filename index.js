03a81eca1fe119dd5fd85bf2d6cd7f83fd22b268
Ranjit Battewad <ranjit.n.battewad@walkingtree.tech>
2024-03-28 08:57:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// This function is called when the Meet API is ready
+function onApiReady(event) {
+    console.log('Ranjit #### Meet API is ready.');
+  }
+  
+  // Add event listener for when the Meet API is ready
+  document.addEventListener('onApiReady', onApiReady);
+  
